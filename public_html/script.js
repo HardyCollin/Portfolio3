35402bb71ff9fef12019c6844ccfce5f5ae5c012
Collin hardy <Collin.Hardy25@gmail.com>
2015-01-08 11:27:46 -0800

    edits

--- public_html/script.js
+++ public_html/script.js
@@ -1,10 +1,27 @@
+///* 
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//
+//
+//$("document").ready(function(){
+//    $(".youtypeinwhatitsis").css("background-color", "blue");
+//});
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-
-$("document").ready(function(){
-    $(".youtypeinwhatitsis").css("background-color", "blue");
+$("document").ready(function() {
+    $(".class1").css("color", "yellow");
+    $("div p:last-child").css("background-color", "blue");
+    $("#alertButton").bind("click", alertButtonClick);
+    
 });
+
+function alertButtonClick() {
+    alert("this alert means a button was clicked");
+}
